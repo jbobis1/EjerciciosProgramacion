c3dfafe6f37ae6866d96797a8bded7b4332fb109
jbobis1 <javibobis12@hotmail.com>
2021-09-27 13:00:47 +0200

    ejercicio

--- src/capitulo01/bloque01/Ejercicio05.java
+++ src/capitulo01/bloque01/Ejercicio05.java
@@ -17,26 +17,26 @@ public class Ejercicio05 {
 		String str = "1732"; // Sacamos ventana en pantalla y obtenemos un String
 		str = JOptionPane.showInputDialog("variable 1: ");
 		int var1 = Integer.parseInt(str); // Convierto la variable escrita en un número entero
-		System.out.println("Número introducido: " + var1); // Muestro el número en pantalla
+	
 		// Se repiten los pasos anteriores
 		
 		str = JOptionPane.showInputDialog("variable 2: ");
 		int var2 = Integer.parseInt(str);
-		System.out.println("Número introducido: " + var2);
-		
 	
-						
 
-		// sumo las variables.
-		
-		int var3 = var1 ;
-		int var4 = var2 ;
 				
 		System.out.println("valor de var1: " + var1 );
 		System.out.println("valor de var2: " + var2 );
-		System.out.println("valor de intercambido var1: " + var4 );
-		System.out.println("valor de intercambido var2: " + var3 );
 		
+		// intercambio de variables
+		int var3;
+		
+		var3 = var1;
+		var1 = var3;
+		var2 = var1;
+		
+		System.out.println("valor de intercambido var1: " + var2 );
+		System.out.println("valor de intercambido var2: " + var1 );
 	}
 	
 }
