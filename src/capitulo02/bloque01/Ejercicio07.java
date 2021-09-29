0b56e801f34e6ed632b2df2060fd9dd895e90c68
jbobis1 <javibetis12@hotmail.com>
2021-09-29 14:50:18 +0200

    Ejercicios clase

--- src/capitulo02/bloque01/Ejercicio07.java
+++ src/capitulo02/bloque01/Ejercicio07.java
@@ -1,5 +1,7 @@
 package capitulo02.bloque01;
 
+import javax.swing.JOptionPane;
+
 //
 //  7º.- Utiliza la operación AND y el operador "if", para tratar de averiguar 
 //  si un número introducido por el usuario es par o impar. Sería recomendable que 
@@ -7,5 +9,21 @@ package capitulo02.bloque01;
 //
 
 public class Ejercicio07 {
+
+	public static void main(String[] args) {
+		String str  = JOptionPane.showInputDialog("Introduzca un numero: ");
+		int numero = Integer.parseInt(str); 
+
+		
+
+		if ((numero & 1) == 0) {
+			System.out.println("El numero: " + numero + " Si es Par");
+		} 
+
+		else {
+			System.out.println("El numero: " + numero + " No es Par");
+		}
+
+	}
 
 }
