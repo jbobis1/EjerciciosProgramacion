b317780499090057184dd159052d06ff8067af6f
jbobis1 <javibobis12@hotmail.com>
2021-10-01 10:29:20 +0200

    clase

--- src/capitulo02/bloque02/Ejercicio02.java
+++ src/capitulo02/bloque02/Ejercicio02.java
@@ -3,11 +3,12 @@ package capitulo02.bloque02;
 import javax.swing.JOptionPane;
 
 //
-//  2�.- Realiza un ejercicio igual al anterior, pero con las dos siguientes diferencias:
-//  no existe la categor�a de n�meros negativos, por tanto si el usuario introduce un
+//  2.- Realiza un ejercicio igual al anterior, pero con las dos siguientes diferencias:
+//  no existe la categoria de numeros negativos, por tanto si el usuario introduce un
 //  negativo, el programa debe terminar; la otra diferencia es que, al acabar, el programa
-//  no debe mostrar la suma de los n�meros, sino cuantos han entrado en cada categor�a.
-//  A esto se llema contadores, a diferencia del ejercicio anterior, que reciben el nombre de acumuladores.
+//  no debe mostrar la suma de los numeros, sino cuantos han entrado en cada categoria.
+//  A esto se llema contadores, a diferencia del ejercicio anterior, que reciben el
+//  nombre de acumuladores.
 //
 
 
