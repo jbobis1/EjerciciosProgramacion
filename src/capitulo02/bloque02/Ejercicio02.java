972fc55ab7cdf375ba28a43947dfe5ca1e28472c
jbobis1 <javibetis12@hotmail.com>
2021-10-03 11:26:33 +0200

    ejercicios casa

--- src/capitulo02/bloque02/Ejercicio02.java
+++ src/capitulo02/bloque02/Ejercicio02.java
@@ -13,137 +13,87 @@ import javax.swing.JOptionPane;
 
 
 public class Ejercicio02 {
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 		
-		String str = JOptionPane.showInputDialog("Introduzca el valor del numero1: ");
-		int num1 = Integer.parseInt(str);
-		
-		str =JOptionPane.showInputDialog("Introduzca el valor del numero2: ");
-		int num2 = Integer.parseInt(str);
-		
-		str = JOptionPane.showInputDialog("Introduzca el valor del numero3: ");
-		int num3 = Integer.parseInt(str);
-		
-		str =JOptionPane.showInputDialog("Introduzca el valor del numero4: ");
-		int num4 = Integer.parseInt(str);
-		
-		str = JOptionPane.showInputDialog("Introduzca el valor del numero5: ");
-		int num5 = Integer.parseInt(str);
-		
-		
-		int bajos = 0;
-		int medios = 0;
-		int grandes = 0;
-		
-		
-		
-		if (num1 < 0) {
-			System.out.println ("Error");
-			 
-		}
-		else { 
-			if ( num1 >=0 && num1 <= 25) { 
-				bajos = bajos + num1;
-			}
-			else 
-				if (num1 >=26 && num1<= 250){ 
-				medios = medios + num1;
-			}
-			else 
-				if (num1  > 250){
-					grandes = grandes + num1;
-			
-				}
-		}
-		
-		if (num2 < 0) {
-			System.out.println ("Error");
-		}
-		else { 
-			if ( num2 >=0 && num2 <= 25) { 
-				bajos = bajos + num2;
-			}
-			else 
-				if (num2 >=26 && num2<= 250){
-					medios = medios + num2;
-			}
-			else 
-				if (num2  > 250){
-					grandes = grandes + num2;
-			
-				}
-		}
-		
-		if (num3 < 0) {
-			System.out.println ("Error");
-		}
-		else { 
-			if ( num3 >=0 && num3 <= 25) { 
-				bajos = bajos + num3;
-			}
-			else 
-				if (num3 >=26 && num3<= 250){
-					medios = medios + num3;
-			}
-			else 
-				if (num3  > 250){
-					grandes = grandes + num3;
-			
-				}
-		}
-		
-		
-		if (num4 < 0) {
-			System.out.println ("Error");
-		}
-		else { 
-			if ( num4 >=0 && num4 <= 25) { 
-				bajos = bajos + num4;
-			}
-			else 
-				if (num4 >=26 && num4<= 250){
-					medios = medios + num4;
-			}
-			else 
-				if (num4  > 250){
-					grandes = grandes + num4;
-			
-				}
-		}
-		
-		if (num5 < 0) {
-			System.out.println ("Error");
-		}
-		else { 
-			if ( num5 >= 0 && num5 <= 25) { 
-				bajos = bajos + num5;
-			}
-			else 
-				if (num5 >= 26 && num5 <= 250){
-					medios = medios + num5;
-			}
-			else 
-				if (num5  > 250){
-					grandes = grandes + num5;
-			
-				}
-			}
-		
-		System.out.println("LOs numeros negativos; " );
-		System.out.println("LOs numeros bajos; " + bajos);
-		System.out.println("LOs numeros medios; " + medios);
-		System.out.println("LOs numeros grandes; " + grandes);
-	
-		
-		
-		
+        Integer numBajo = 0;
+        Integer numMedio = 0;
+        Integer numAlto = 0;
+        System.out.println("Introduzca el valor del numero1: ");
+        
+        
+		String str1 = JOptionPane.showInputDialog("Introduzca el valor del numero1: ");
+		int num1 = Integer.parseInt(str1);
+
+		if(num1 < 0) {
+            System.out.println("La cantidad de número introducidos ha sido:\nNumeros bajos: " + numBajo + "\nNumeros medios: " + numMedio + "\nNumeros altos: "+numAlto);
+		} else {
+            if(num1 >= 0 && num1 <= 25) {
+                numBajo++;
+            } else if(num1 >= 26 && num1 <= 250) {
+                numMedio++;
+            } else {
+                numAlto++;
+            }
+ 		    String str2 = JOptionPane.showInputDialog("Introduzca el valor del numero2: ");
+		    int num2 = Integer.parseInt(str2);
+            if(num2 <0) {
+                System.out.println("La cantidad de número introducidos ha sido:\nNumeros bajos: " + numBajo + "\nNumeros medios: " + numMedio + "\nNumeros altos: "+numAlto);
+            } else {
+            	 if(num2 >= 0 && num2 <= 25) {
+                     numBajo++;
+                 } else if(num2 >= 26 && num2 <= 250) {
+                     numMedio++;
+                 } else {
+                     numAlto++;
+                 }
+            	
+            	String str3 = JOptionPane.showInputDialog("Introduzca el valor del numero3: ");
+     		    int num3 = Integer.parseInt(str3);
+                if(num3<0) {
+                	System.out.println("La cantidad de número introducidos ha sido:\nNumeros bajos: " + numBajo + "\nNumeros medios: " + numMedio + "\nNumeros altos: "+numAlto);
+                } else {
+                	 if(num3 >= 0 && num3 <= 25) {
+                         numBajo++;
+                     } else if(num3 >= 26 && num3 <= 250) {
+                         numMedio++;
+                     } else {
+                         numAlto++;
+                     }
+                	
+          		    String str4 = JOptionPane.showInputDialog("Introduzca el valor del numero4: ");
+        		    int num4 = Integer.parseInt(str4);
+                    if(num4 <0) {
+                        System.out.println("La cantidad de número introducidos ha sido:\nNumeros bajos: " + numBajo + "\nNumeros medios: " + numMedio + "\nNumeros altos: "+numAlto);
+                    } else {
+                    	 if(num4 >= 0 && num4 <= 25) {
+                             numBajo++;
+                         } else if(num4 >= 26 && num4 <= 250) {
+                             numMedio++;
+                         } else {
+                             numAlto++;
+                         }
+                    	
+               		    String str5 = JOptionPane.showInputDialog("Introduzca el valor del numero4: ");
+            		    int num5 = Integer.parseInt(str5);
+                        if(num5 < 0) {
+                            System.out.println("La cantidad de número introducidos ha sido:\nNumeros bajos: " + numBajo + "\nNumeros medios: " + numMedio + "\nNumeros altos: "+numAlto);
+                        } else {
+                        	 if(num5 >= 0 && num5 <= 25) {
+                                 numBajo++;
+                             } else if(num5>= 26 && num5 <= 250) {
+                                 numMedio++;
+                             } else {
+                                 numAlto++;
+                             }
+                        	
+                            
+                        }
+                    }
+                }
+            }
+        }
 		
 	}
-
 	
-	
-	
-	
-	
-	
+		
 }
