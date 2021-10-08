8a23f2d84d0778665fde1c48e18100bd0a09b08d
jbobis1 <javibetis12@hotmail.com>
2021-10-08 19:28:30 +0200

    casa

--- src/capitulo03/bloque02/Ejercicio02.java
+++ src/capitulo03/bloque02/Ejercicio02.java
@@ -6,25 +6,28 @@ public class Ejercicio02 {
 	
 	public static void main (String args[]) {
 		int cantidad = Integer.parseInt(JOptionPane.showInputDialog("Cantidad de numero que quieres: "));
-		int numero;	
-		int mayor = 0;
-		int menor = 0;
+		int numero = 0;	
+		int mayor;
+		int menor;
+		
+		numero = Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero: "));
+		 mayor = numero;
+		 menor = numero;
 				
-		for (int i = 0; i < cantidad; i++) {
+		for (int i = 0; i < cantidad -1; i++) {
 			 numero = Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero: "));
-			 numero = mayor;
-			 numero = menor;
 			 
-				if (numero < mayor) {
-					mayor = numero ;		
+				if (numero >= mayor) {
+					mayor = numero;		
 				}	
 	
-				else if(numero > menor) {
+				else
+					{
 					menor = numero;
 				}
 				
-				System.out.println( "El numero Mayor es: " + mayor + " El numero Menor es: " + menor );
+				
 		}
-		
+		System.out.println( "El numero Mayor es: " + mayor + " El numero Menor es: " + menor );
 	}	
 }
