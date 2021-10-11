b1e6d747b28c54c39c640dcfe0b7c19b83575b6f
jbobis1 <javibetis12@hotmail.com>
2021-10-11 19:52:53 +0200

    casa

---
+++ src/capitulo03/bloque03/Ejercicio07.java
@@ -0,0 +1,36 @@
+package capitulo03.bloque03;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio07 {
+	
+		public static void main(String args[]) {
+			
+			int numero = Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero: ")); ;
+			int negativos = 0;
+			int positivos = 0;
+
+		
+			for (;numero != 0;) {
+					numero = Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero: "));
+							 
+				if (numero >0) {
+					positivos++;
+				}	
+				else	
+					if (numero <0) {
+						negativos++;
+				}	
+					
+					else{
+						
+					}
+			}
+
+			System.out.println( "Positivos: " + positivos + " Negativos : " + negativos );
+
+		}
+
+	
+
+}
