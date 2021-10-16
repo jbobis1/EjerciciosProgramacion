d2107c45434448c03af71a8f3e2a38f1101dfc42
jbobis1 <javibetis12@hotmail.com>
2021-10-16 19:10:51 +0200

    casa

--- src/capitulo03/bloque04/Ejercicio02.java
+++ src/capitulo03/bloque04/Ejercicio02.java
@@ -3,28 +3,27 @@ package capitulo03.bloque04;
 import javax.swing.JOptionPane;
 
 public class Ejercicio02 {
-
+	
 	public static void main (String args[]) {
+		boolean primo = true;
 		int minimo = Integer.parseInt(JOptionPane.showInputDialog("Introduzca numero Minimo: "));
 		int maximo = Integer.parseInt(JOptionPane.showInputDialog("Introduzca numero Maximo: "));
-		int numero = minimo;
-		int contador = 2;
+		int i = 2;
 		
-	
 		
-		while (numero <= maximo) {
-			if((numero % contador == 0)) {
-				
-				System.out.println("El nuero " + numero + " no primo");	
-				contador++;
-			}
-			else {
-				
-				System.out.println("El numero " + numero + " es primo");
-				
-			}
-		
-			numero++;
+		while (minimo <= maximo) {
+			int num = minimo;
+			
+			while (i<= num /2 && primo) {
+				if(num % i == 0)
+					primo = false;
+				i++;
+			}		
+			i = 2;
+			if (primo) System.out.println("El numero " + num + " es primo");
+			else System.out.println("El numero " + num + " no primo");
+			primo =true;
+			minimo++;
 		}
 	}
 }
