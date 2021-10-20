69cea681f72b3c2e11dadffcfbedba145d8f1ff3
jbobis1 <javibobis12@hotmail.com>
2021-10-20 11:19:27 +0200

    arrays

--- src/capitulo05/bloque01/Ejercicio03.java
+++ src/capitulo05/bloque01/Ejercicio03.java
@@ -2,24 +2,21 @@ package capitulo05.bloque01;
 
 public class Ejercicio03 {
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		int numeros[] = new int[150];
 		
-		System.out.print("No invertidos ");
+		System.out.print("-No Invertidos: ");
 		for (int i = 0; i < numeros.length; i++) {
 			numeros[i] = (int)Math.round(Math.random() * (150 - 0)) + 0;
 			System.out.print(numeros[i] + " ");
 		}
-
-		System.out.print( "\nNumero invertidos ");
-		   for (int i = 0; i < numeros.length/2; i++) {
-		        int aux = numeros[i];
-		        numeros[i] = numeros[numeros.length - 1 - i];
-		        numeros[numeros.length - 1 - i] = aux;
-		        
-		    	System.out.print( + numeros[i] + " ");
-		    }
+		
+		System.out.print( "\n ---Invertidos: ");
+		
+		for (int i = 149; i >= 0;) {
+			System.out.print( + numeros[i] + " ");
+			i--;
+		}
 	}
 }
 	
--- src/capitulo05/bloque01/Ejercicio05.java
+++ src/capitulo05/bloque01/Ejercicio05.java
@@ -6,19 +6,18 @@ public class Ejercicio05 {
 	int pares = 0;
 	int impares = 0;
 	
-	// Inicialización de los valores del array
 		for (int i = 0; i < numeros.length; i++) {
 			numeros[i] = (int)Math.round(Math.random() * (150 - 0)) + 0;
-			System.out.print(numeros[i] + " ");
+		
 			
-			 	if ((numeros[0-150] % 2) == 0) {
-			 		pares = pares + numeros;
-			        System.out.println("El numero: " + pares + " Si es Par");
+			 	if ((numeros[i] % 2) == 0) { 
+			 		pares = pares + numeros[i];
 			    } 
 			    else {
-			    	impares = impares+numeros;
-			        System.out.println("El numero: " + impares + " No es Par");
+			    	impares = impares + numeros[i];
 			    }
 		}
+		 System.out.println("Suma de numero Impares: " + impares );
+		 System.out.println("Suma de numero Pares: " + pares  );
 	}	
 }
