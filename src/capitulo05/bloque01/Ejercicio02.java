5596e74228f643eb29950b346115b3e27e9eefb6
jbobis1 <javibobis12@hotmail.com>
2021-10-20 11:26:44 +0200

    arrays

--- src/capitulo05/bloque01/Ejercicio02.java
+++ src/capitulo05/bloque01/Ejercicio02.java
@@ -8,14 +8,11 @@ public class Ejercicio02 {
 		int inferior = Integer.parseInt(JOptionPane.showInputDialog("Limite inferior: "));
 		int superior = Integer.parseInt(JOptionPane.showInputDialog("Limite inferior: "));
 		int numeros[] = new int[150];
-		
-		
+
 		// Inicialización de los valores del array
 		for (int i = 0; i < numeros.length; i++) {
 			numeros[i] = (int)Math.round(Math.random() * (superior - inferior)) + inferior;
 			System.out.print(numeros[i] + " ");
 		}
-	
 	}
-	
 }
--- src/capitulo05/bloque01/Ejercicio06.java
+++ src/capitulo05/bloque01/Ejercicio06.java
@@ -1,5 +1,22 @@
 package capitulo05.bloque01;
 
 public class Ejercicio06 {
+	public static void main(String[] args) {
+	int numeros[] = new int[150];
+	int suma = 0;
 
+	
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = (int)Math.round(Math.random() * (150 - 0)) + 0;
+		
+			
+			 	if ((numeros[i] = 0) { 
+			 		suma = suma + numeros[i];
+			 		
+			    } 
+
+		}
+		 System.out.println("Suma de numero: " + suma );
+	
+	}	
 }
