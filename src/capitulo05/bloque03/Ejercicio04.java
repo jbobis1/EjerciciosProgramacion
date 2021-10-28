faf355d2c5cb60f50c59145d2cdccab5b224a1b7
jbobis1 <javibobis12@hotmail.com>
2021-10-28 14:51:55 +0200

    arrays

--- src/capitulo05/bloque03/Ejercicio04.java
+++ src/capitulo05/bloque03/Ejercicio04.java
@@ -2,7 +2,7 @@ package capitulo05.bloque03;
 
 public class Ejercicio04 {
 	public static void main(String[] args) {
-		
+		int masPequeño,j,temp;
 		int numeros[] = new int[150];
 		
 		
@@ -10,6 +10,15 @@ public class Ejercicio04 {
 		for (int i = 0; i < numeros.length; i++) {
 			numeros[i] = (int)Math.round(Math.random() * (1000 - 0)) + 0;
 		}
+		for(int i=0; i<numeros.length-1;i++) {
+			masPequeño=i;
+			
+			for(j = i; j < numeros.length; j++) {
+					if()
+			}
+				
+		}
+		
 	
 	}
 }
