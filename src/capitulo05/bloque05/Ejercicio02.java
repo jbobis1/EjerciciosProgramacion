d682f4b079443acd604f0d1a8c4d3ab654e2ba96
jbobis1 <javibetis12@hotmail.com>
2021-11-09 16:18:16 +0100

    arrays

--- src/capitulo05/bloque05/Ejercicio02.java
+++ src/capitulo05/bloque05/Ejercicio02.java
@@ -2,28 +2,34 @@ package capitulo05.bloque05;
 
 public class Ejercicio02 {
 	public static void main(String[] args) {
-		float array[] = new float[20];
-		System.out.println("Array Aleatorio:");
-		// Inicialización de los valores del array
+		int array[] = new int[20];
+		
+		empezararray(array);
+		calculoNota(array);
+	}
+	
+	public static void empezararray(int array[]){	
+			
 		for (int i = 0; i < array.length; i++) {
-			array[i] = (float)Math.round(Math.random() * (10.00 - 0.00)) + 0;
-			   System.out.print(array[i]+"-");
+			array[i] = (int)Math.round(Math.random() * (10 - 0)) + 0;	   
 		}
-         
-		System.out.println("");
+	} 
+	
+	public static void calculoNota(int[]array){
+		int aprobado=0, suspendo=0;
+		
 	    for (int i = 0; i < array.length; i++) {
 	    	
 	    	if(array[i]>=5) {
-	    		System.out.println("");
-	    		 System.out.print("Aprobado: "+ array[i]);
-	    		
+	    		aprobado++;	
 	    	}
-	    	if(array[i] <=5) {
-	    		System.out.println("");
-	    		 System.out.print("Suspenso: "+ array[i]);
+	    	else{
+	    		suspendo++;
 	    		 
 	    	}
-	    	
 	    }
+	    System.out.println("Porcentaje aprobados: "+ (aprobado*100)/array.length);
+	    System.out.println("Porcentaje aprobados: "+ (suspendo*100)/array.length);
+
     }
 }
