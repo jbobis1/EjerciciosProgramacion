081e02bc8666a3bf81742c03a41ac18024f57a39
jbobis1 <javibobis12@hotmail.com>
2021-11-15 12:58:32 +0100

    array

--- src/capitulo05/bloque06/Ejercicio01.java
+++ src/capitulo05/bloque06/Ejercicio01.java
@@ -3,8 +3,14 @@ package capitulo05.bloque06;
 public class Ejercicio01 {
 	public static void main(String[] args) {
 		int array[] = new int[7];
-
-       
+		
+		crear(array); 
+		mostrar(array);
+		
+	}
+	
+	
+	private static void crear(int[] array) {
 		System.out.println("Array Aleatorio:");
 		// creamos el bucle de creacion del array
 		for (int i = 0; i < array.length; i++) {
@@ -15,10 +21,13 @@ public class Ejercicio01 {
         		i--;
         		}
 		  	}      
-		}  
+		} 
+	}
+	private static void mostrar(int[] array) {
 		// recorremos el array para ya mostrarlo
 		for (int i = 0; i < array.length; i++) {
-			   System.out.print(array[i]+"-");
+			 
 		}	
     }
+
 }
--- src/capitulo05/bloque06/Ejercicio02.java
+++ src/capitulo05/bloque06/Ejercicio02.java
@@ -2,4 +2,16 @@ package capitulo05.bloque06;
 
 public class Ejercicio02 {
 
-}
+	public static void main(String[] args) {
+		char matriz[][] = new char[10][30]; 	
+
+		
+		}
+	
+	
+		
+	
+	
+	}
+	
+
