843449d50b4c210753384a8c0ea5a9033eb7ff1c
jbobis1 <javibetis12@hotmail.com>
2021-11-15 18:18:54 +0100

    arrays

--- src/capitulo05/bloque06/Ejercicio02.java
+++ src/capitulo05/bloque06/Ejercicio02.java
@@ -5,13 +5,22 @@ public class Ejercicio02 {
 	public static void main(String[] args) {
 		char matriz[][] = new char[10][30]; 	
 
-		
+		for (int i = 0; i< matriz.length; i++) {		
+			for (int j = 0; j< matriz.length; j++) {
+				matriz [i][j] = "*";
+				do {
+					matriz [i][j] = "*";
+					System.out.println("*");
+				}while (j <(int)matriz [i][j]);
+				do {
+					System.out.println(" ");
+				}while (j <(int)matriz [i][j]);
+				System.out.println(matriz [i][j]+"\t");
+			}
+			System.out.println();
 		}
-	
-	
-		
-	
-	
-	}
+	}	
+}
+
 	
 
