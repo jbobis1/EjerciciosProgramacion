fbdc2e8fc3ac75e8d96a3bc798f57244e350e76f
jbobis1 <javibetis12@hotmail.com>
2021-11-16 15:38:58 +0100

    arrays

--- src/capitulo05/bloque06/Ejercicio04.java
+++ src/capitulo05/bloque06/Ejercicio04.java
@@ -5,20 +5,22 @@ public class Ejercicio04 {
 		char matriz[][] = new char[20][10]; 
 		int asteriscos, fila, columna;
 		
-		
 		for (int i = 0; i< matriz.length; i++) {
 			//Indiciamos el número de asteriscos en cada fila
 			fila = (int) ( Math.round(Math.random() *20));	
 			columna = (int) ( Math.round(Math.random() *10));
 
-
 			for (int j = 0; j < matriz[i].length; j++) {
 			//Hay que meter los asteriscos en la matriz	
-				matriz[i][j]=matriz[fila][columna];
+				
 				if (i == 0 || i == matriz.length-1 || j == 0 || j == matriz[i].length-1) {
 					matriz[i][j] = '*';
 				}
 				
+				if (i == fila || j == columna) {
+					matriz[i][j] = '0';
+				}	
+		
 				else {
 					matriz[i][j]=' ';
 				}
--- src/capitulo05/bloque06/Ejercicio05.java
+++ src/capitulo05/bloque06/Ejercicio05.java
@@ -1,5 +1,27 @@
 package capitulo05.bloque06;
 
 public class Ejercicio05 {
+	public static void main(String[] args) {
+		char matriz[][] = new char[20][10]; 
+		int asteriscos, fila, columna;
+		int cerouno;
+		
+		for (int i = 0; i< matriz.length; i++) {
+			//Indiciamos el número de asteriscos en cada fila
 
+			for (int j = 0; j < matriz[i].length; j++) {
+			//Hay que meter los asteriscos en la matriz	
+				
+				if (i == 0 || i == matriz.length-1 || j == 0 || j == matriz[i].length-1) {
+					matriz[i][j] = '*';
+				}
+				else {			
+					cerouno=(char) ( Math.round(Math.random() * (1 - 0)) + 0);	
+					matriz[i][j] = (char) cerouno;
+				}
+				System.out.print(matriz[i][j] + " ");
+			}
+			System.out.println(" ");
+			}	
+		}
 }
