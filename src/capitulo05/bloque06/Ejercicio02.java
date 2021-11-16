988e8d7e248ef7045fad74c855d11507288e87eb
jbobis1 <javibobis12@hotmail.com>
2021-11-16 12:33:43 +0100

    arrays

--- src/capitulo05/bloque06/Ejercicio02.java
+++ src/capitulo05/bloque06/Ejercicio02.java
@@ -3,24 +3,18 @@ package capitulo05.bloque06;
 public class Ejercicio02 {
 
 	public static void main(String[] args) {
-		char matriz[][] = new char[10][30]; 	
-
-		for (int i = 0; i< matriz.length; i++) {		
-			for (int j = 0; j< matriz.length; j++) {
-				matriz [i][j] = "*";
-				do {
-					matriz [i][j] = ;
-					System.out.println("*");
-				}while (j <(int)matriz [i][j]);
-				do {
-					System.out.println(" ");
-				}while (j <(int)matriz [i][j]);
-				System.out.println(matriz [i][j]+"\t");
+		char matriz[][] = new char[10][30]; 
+		int asteriscos;
+		for (int i = 0; i< matriz.length; i++) {
+			//Indiciamos el número de asteriscos en cada fila
+			asteriscos = (int) ( Math.round(Math.random() *30));		
+			for (int j = 0; j<asteriscos; j++) {
+			//Hay que meter los asteriscos en la matriz			
+				matriz[i][j]='*';
+				System.out.print(matriz[i][j] + " ");		
 			}
-			System.out.println();
+			System.out.println(" ");
+			}	
 		}
 	}	
-}
-
-	
 
