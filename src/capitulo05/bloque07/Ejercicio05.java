c8ed9ce69b5aa2425740a6da4bd91631ec596a9c
jbobis1 <javibetis12@hotmail.com>
2021-11-21 14:01:22 +0100

    arrays

--- src/capitulo05/bloque07/Ejercicio05.java
+++ src/capitulo05/bloque07/Ejercicio05.java
@@ -3,28 +3,35 @@ package capitulo05.bloque07;
 import javax.swing.JOptionPane;
 
 public class Ejercicio05 {
-
 		public static void main(String[] args) {
-		int n = 1;
 		int array[] = new int [] {1,2,6,1,9,1};
-		int contador=0;
-		countOccurrences(array, contador, n);
+		countOccurrences(array);
 //		System.out.print(findAndReplace(array));
 		}
-
-		private static int[] countOccurrences(int[] array,int contador,int n ) {
+		
+		private static int[] countOccurrences(int[] array) {
+			int longitud =array.length;
 			
-			int k=0;
-			int nuevoarray[] = new int [] {};
-			  for(int i = 0; i < array.length; i++){  
-				  
-				  if (array[i] != n) {		 
-					  nuevoarray[k]=array[i];
-					  k++;
-					  System.out.println(nuevoarray[k]);
-				  } 
-//				  System.out.println("Numero de veces que se repite el numero es: " + nuevoarray[k]);
-			  }	 
-			  return nuevoarray;
+			int n = 1;
+			int contador=0;
+//			int nuevoarray[] = new int [] {};
+			
+			for (int i = 0; i < array.length; i++) {
+	            if (n == array[i]) {
+	                for (int j = i; j < (array.length - 1); j++){
+	                    array[j] = array[j + 1];
+	                }
+	                longitud--;
+	                i--;
+	                contador++;
+	            }
+	        }
+
+	        if (contador >= 1) {
+	            for (int i = 0; i < array.length - contador; i++) {
+	                System.out.print(array[i] + " ");	  
+	            }
 		}
-}	
+			return array;
+		}	
+}
