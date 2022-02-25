824b4cfce102d1633f4fc436881332d83d2a6453
jbobis1 <javibobis12@hotmail.com>
2022-02-25 12:59:38 +0100

    capitulo 06 bloque 02

--- src/capitulo06/bloque02/Ejercicio04.java
+++ src/capitulo06/bloque02/Ejercicio04.java
@@ -1,17 +1,27 @@
 package capitulo06.bloque02;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.TimeZone;
+
+import javax.swing.JOptionPane;
 
 public class Ejercicio04 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
-	
+		Date date = null;
+		String patron = "yyyy-MM-dd";
 		
+		String fecha = (JOptionPane.showInputDialog("2012-05-11: ")) ;
+
+		SimpleDateFormat sdf = new SimpleDateFormat(patron);
+		date = sdf.parse("2012-05-11");
+				
+		Calendar calenda = new Calendar ("2012-05-11");
 		
 	}
-
-	
 
 }
