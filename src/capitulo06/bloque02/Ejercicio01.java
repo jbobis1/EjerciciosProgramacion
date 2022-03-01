c6003d500c592d5312d4885dc314b16155f5f7ce
jbobis1 <javibobis12@hotmail.com>
2022-03-01 17:41:50 +0100

    capitulo06

--- src/capitulo06/bloque02/Ejercicio01.java
+++ src/capitulo06/bloque02/Ejercicio01.java
@@ -3,16 +3,15 @@ package capitulo06.bloque02;
 public class Ejercicio01 {
 
 	public static void main(String[] args) {
-
-		
 		
 		for (float x=0; x<1; x+=0.001) {
+			
 			double y1=Math.sqrt(x);
 			double y2=Math.log(x);
 			
 			
-			if (Math.abs(y1-y2)<0.001) {
-				double dis=y1-y2;
+			if (Math.abs( y1 - y2) < 0.001) {
+				System.out.println("el punto medio es"+Math.abs( y1 - y2));
 				
 			}
 		}
