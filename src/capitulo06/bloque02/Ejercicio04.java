5c6d6fa6674f7527f630ff37b035e83b42d4720d
jbobis1 <javibobis12@hotmail.com>
2022-03-02 12:14:23 +0100

    capitulo 06

--- src/capitulo06/bloque02/Ejercicio04.java
+++ src/capitulo06/bloque02/Ejercicio04.java
@@ -13,20 +13,34 @@ public class Ejercicio04 {
 
 	public static void main(String[] args) throws ParseException {
 
-	
 		String patron = "yyyy-MM-dd";
 		
 		String fecha = (JOptionPane.showInputDialog("19/03/1977 15:33:00")) ;
 
 		SimpleDateFormat sdf = new SimpleDateFormat(patron);
-		
-		Date date = sdf.parse(fecha);
-				
 	
+		Date date = sdf.parse(fecha);
+		
 		Calendar fechaCalendar = new GregorianCalendar(1918, Calendar.APRIL, 01, 22, 05, 23);
+		fechaCalendar.get(Calendar.YEAR);
+		fechaCalendar.get(Calendar.MONTH);
+		fechaCalendar.get(Calendar.WEEK_OF_MONTH);
+		fechaCalendar.get(Calendar.HOUR_OF_DAY);
 		
+		SimpleDateFormat  sdf2 = new SimpleDateFormat("yyyy");
 		
-		
+			Date años = sdf.parse("yyyy");
+			sdf2.format(años);
+			
+			Date mes = sdf.parse("MM");
+			sdf2.format(mes);
+			
+			Date dia = sdf.parse("dd");
+			sdf2.format(dia);
+			
+			Date hora = sdf.parse("HH:mm:ss");
+			sdf2.format(hora);
+		 
 	}
 
 }
